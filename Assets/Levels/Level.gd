ea0fac9e2b4266d75614b5278a4bf62efb982209
jawdan-dev <34403286+jawdan-dev@users.noreply.github.com>
2024-07-25 20:57:38 +0100

    Camera zoom is now uniform.

--- Assets/Levels/Level.gd
+++ Assets/Levels/Level.gd
@@ -1,12 +1,10 @@
 extends Node2D
 
 func _ready():
-	var tileSize = $Walls.tile_set.tile_size;
-	var offset = Vector2($Walls.get_used_rect().position * tileSize);
-	var size = Vector2($Walls.get_used_rect().size * tileSize) ;
-	$Camera.global_position = $Walls.global_position + (size / 2.0) + offset;
-	$Camera.zoom = Vector2(get_viewport_rect().size) / (size - (tileSize * 2.0));
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	var tileSize = $Objects/Walls.tile_set.tile_size;
+	var offset = Vector2($Objects/Walls.get_used_rect().position * tileSize);
+	var size = Vector2($Objects/Walls.get_used_rect().size * tileSize) ;
+	$Camera.global_position = $Objects/Walls.global_position + (size / 2.0) + offset;
+	var zoomRaw = Vector2(get_viewport_rect().size) / (size - (tileSize * 2.0));
+	var zoom = min(zoomRaw.x, zoomRaw.y);
+	$Camera.zoom = Vector2(zoom, zoom); 
